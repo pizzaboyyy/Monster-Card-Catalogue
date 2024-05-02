25e3fb988012ee179ff9efb23a47a4be578b5802
pizzaboyyy <121211168+pizzaboyyy@users.noreply.github.com>
2024-05-02 15:15:23 +1200

    2_storing_cards
    trailing Using lists to store

--- 1_storing_cards.py
+++ 1_storing_cards.py
@@ -64,71 +64,18 @@ cards_dict = {
 }
 
 
-# Trial 2 Using List
-cards_list = {
-    "Stoneling": [
-        ["Strength", 7],
-        ["Speed", 1],
-        ["Stealth", 25],
-        ["Cunning", 15],
-    ],
-    "Vexscream": [
-        ["Strength", 1],
-        ["Speed", 6],
-        ["Stealth", 21],
-        ["Cunning", 19],
-    ],
-    "Dawnmirage": [
-        ["Strength", 5],
-        ["Speed", 15],
-        ["Stealth", 18],
-        ["Cunning", 22]
-    ],
-    "Blazegolem": [
-        ["Strength", 15],
-        ["Speed", 20],
-        ["Stealth", 23],
-        ["Cunning", 6]
-    ],
-    "Websnake": [
-        ["Strength", 7],
-        ["Speed", 15],
-        ["Stealth", 10],
-        ["Cunning", 5]
-    ],
-    "Moldvine": [
-        ["Strength", 21],
-        ["Speed", 18],
-        ["Stealth", 14],
-        ["Cunning", 5]
-    ],
-    "Vortexwing": [
-        ["Strength", 19],
-        ["Speed", 13],
-        ["Stealth", 19],
-        ["Cunning", 2]
-    ],
-    "Rotthing": [
-        ["Strength", 16],
-        ["Speed", 7],
-        ["Stealth", 4],
-        ["Cunning", 12]
-    ],
-    "Froststep": [
-        ["Strength", 14],
-        ["Speed", 14],
-        ["Stealth", 17],
-        ["Cunning", 4]
-    ],
-    "Wispghoul": [
-        ["Strength", 17],
-        ["Speed", 19],
-        ["Stealth", 3],
-        ["Cunning", 2]
-    ]
-}
 
 
-for card_name, card_menu in cards_dict.items():
-    for card_item, item_value in card_menu.items():
-        easygui.msgbox(card_name, card_item, item_value)
+
+for card_name, attributes in cards_dict.items():
+    print(f"Card: {card_name}")
+    print(f"   Strength: {attributes['Strength']}")
+    print(f"   Speed: {attributes['Speed']}")
+    print(f"   Stealth: {attributes['Stealth']}")
+    print(f"   Cunning: {attributes['Cunning']}")
+    print()
+
+
+# for card_name, card_menu in cards_dict.items():
+ #   for card_item, item_value in card_menu.items():
+  #      print(card_name, card_item, item_value)
